5c149ea21cebcdf90d5280031dd615268ee83983
Corneliu <29034380+mishimomei@users.noreply.github.com>
2023-06-21 17:09:36 +0300

    added operate function, Assignment 3.

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-let add = (a, b) => a + b;
-let substract = (a, b) => a - b;
-let multiply = (a, b) => a * b;
-let divide = (a, b) => a / b;
+const add = (a, b) => a + b;
+const substract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+let firstNumber;
+let operator;
+let secondNumber;
+
+const operate = (op, a, b) => {
+    switch(op) {
+        case '+': {
+            add(a, b);
+            break;
+        } case '-': {
+            substract(a, b);
+            break;
+        } case '*': {
+            multiply(a, b);
+            break;
+        } case '/': {
+            divide(a, b);
+            break;
+        }
+    }
+}
