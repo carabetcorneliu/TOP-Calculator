7aa2a3fb426ae1420555f0de9127c1b54960bee9
Corneliu <29034380+mishimomei@users.noreply.github.com>
2023-06-23 10:54:47 +0300

    Assignment 6, added functions operate, displayTextUpdate and displayResult, configured parsing of firstNumber,operator,secondNumber, main functionality finished, Calculator works.

--- script.js
+++ script.js
@@ -6,21 +6,101 @@ const divide = (a, b) => a / b;
 let firstNumber;
 let operator;
 let secondNumber;
+let result;
 
 const operate = (op, a, b) => {
-    switch(op) {
+    switch (op) {
         case '+': {
-            add(a, b);
+            result = add(+a, +b);
             break;
         } case '-': {
-            substract(a, b);
+            result = substract(a, b);
             break;
         } case '*': {
-            multiply(a, b);
+            result = multiply(a, b);
             break;
         } case '/': {
-            divide(a, b);
+            result = divide(a, b);
             break;
         }
     }
 }
+
+const displayResult = () => {
+    displayContainerOne.textContent = "";
+    displayContainerTwo.textContent = "";
+    displayContainerThree.textContent = "";
+    displayContainerResult.textContent = result;
+}
+
+const displayContainerOne = document.querySelector('#displayContainerOne');
+const displayContainerTwo = document.querySelector('#displayContainerTwo');
+const displayContainerThree = document.querySelector('#displayContainerThree');
+const displayContainerResult = document.querySelector('#displayContainerResult');
+
+const displayTextUpdate = () => {
+    if (firstNumber) {
+        displayContainerOne.textContent = firstNumber;
+    }
+    if (operator) {
+        displayContainerTwo.textContent = " " + operator + " ";
+    }
+    if (secondNumber) {
+        displayContainerThree.textContent = secondNumber;
+    }
+}
+
+const buttons = document.querySelectorAll('.buttons');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (!operator) {
+            switch (button.id) {
+                case 'one':
+                case 'two':
+                case 'three':
+                case 'four':
+                case 'five':
+                case 'six':
+                case 'seven':
+                case 'eight':
+                case 'nine':
+                case 'zero':
+                    if (!firstNumber) firstNumber = button.textContent;
+                    else firstNumber += button.textContent;
+                    displayTextUpdate();
+                    break;
+                case 'add':
+                case 'substract':
+                case 'multiply':
+                case 'divide':
+                    operator = button.textContent;
+                    displayTextUpdate();
+                    break;
+            }
+        }
+        else if (operator) {
+            switch (button.id) {
+                case 'one':
+                case 'two':
+                case 'three':
+                case 'four':
+                case 'five':
+                case 'six':
+                case 'seven':
+                case 'eight':
+                case 'nine':
+                case 'zero':
+                    if (!secondNumber) secondNumber = button.textContent;
+                    else secondNumber += button.textContent;
+                    displayTextUpdate();
+                    break;
+                case 'equal':
+                    operate(operator, firstNumber, secondNumber);
+                    displayResult();
+                    break;
+            }
+        }
+    });
+});
+
